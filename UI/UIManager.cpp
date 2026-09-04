0511239e8e741bd9155f0acb7b3aa8f4700ee88b
sakana619 <a.rihito619@icloud.com>
2026-09-04 17:06:04 +0900

    UI管理クラスの実装

---
+++ UI/UIManager.cpp
@@ -0,0 +1,28 @@
+#include "UIManager.h"
+
+UIManager::UIManager()
+{
+}
+
+UIManager::~UIManager()
+{
+}
+
+void UIManager::Init()
+{
+
+}
+
+void UIManager::Update(float deltaTime)
+{
+}
+
+void UIManager::Draw()
+{
+
+}
+
+void UIManager::End()
+{
+
+}
---
+++ UI/UIManager.h
@@ -0,0 +1,78 @@
+#pragma once
+#include<vector>
+#include<memory>
+#include<type_traits>
+
+#include"UIBase.h"
+
+/// <summary>
+/// UIを管理するクラス
+/// </summary>
+class UIManager
+{
+
+public:
+
+	UIManager();
+	~UIManager();
+
+	void Init();
+	void Update(float deltaTime);
+	void Draw();
+	void End();
+
+	/// <summary>
+	/// UIの生成を行う
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <typeparam name="...Args"></typeparam>
+	/// <param name="...args"></param>
+	template<class T,class ...Args>
+	void CreateUI(Args&&... args);
+
+	/// <summary>
+	/// 指定のUIを探す
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <returns></returns>
+	template<class T>
+	T* FindUI();
+
+private:
+
+	/// <summary>
+	/// 管理しているUI
+	/// </summary>
+	std::vector<std::unique_ptr<UIBase>>m_UIList;
+
+};
+
+template<class T, class ...Args>
+inline void UIManager::CreateUI(Args&& ...args)
+{
+	//継承しているかチェック
+	static_assert(std::is_base_of<UIBase, T>::value, "is not base of UIBase");
+	//リストに追加
+	m_UIList.emplace_back(std::make_unique<T>(std::forward<Args>(args)...));
+
+}
+
+template<class T>
+inline T* UIManager::FindUI()
+{
+
+	//継承しているかチェック
+	static_assert(std::is_base_of<UIBase, T>::value, "is not base of UIBase");
+
+	for (const auto& ui : m_UIList) {
+
+		T* check = dynamic_cast<T*>(ui.get());
+		//ダウンキャストが失敗していたら処理しない
+		if (check == nullptr)continue;
+		//見つかったものを返す
+		return check;
+	}
+	//見つからなかったら
+	return nullptr;
+
+}
